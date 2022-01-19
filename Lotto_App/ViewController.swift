90fede14136014df092df7372e8ba7df7d4df9e3
sooy0301 <sooy0301@gmail.com>
2022-01-19 22:53:43 +0900

    Initial Commit

---
+++ Lotto_App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lotto_App
+//
+//  Created by 박수영 on 2022/01/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
