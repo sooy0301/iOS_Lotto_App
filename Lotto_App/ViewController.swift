362d33cf4b431d94666969c0cc64b552a5ee4b74
Park_SY <0301psy@naver.com>
2022-02-01 19:46:28 +0900

    app plan

--- Lotto_App/ViewController.swift
+++ Lotto_App/ViewController.swift
@@ -11,9 +11,14 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        // Do any additional setup after loading the view
+        // 이제 앱 만들어야 하는데
+        // 1. 기능 정의
+        // 2. 스토리보드 + 유스케이스
+        // 3. 시퀀스 다이어그램
+        // 4. ERD // DB설계
+        // 5. 작업 시작
     }
 
 
 }
-
